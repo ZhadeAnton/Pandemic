b0458273ac50e133e0c0ecf476fe23adaecb7a46
ElGutiar <el12guitar@gmail.com>
2021-05-18 23:16:15 +0300

    Added main slider with arrows. Almost finished responce

--- src/Routes/MainPage/MainPage.tsx
+++ src/Routes/MainPage/MainPage.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 import './mainPage.scss'
 import Header from '../../components/Header/Header'
 import Elementor from '../../components/Elementor/Elementor'
+import MainSlider from '../../components/MainSlider/MainSlider'
 
 function MainPage() {
   return (
     <section className='mainPage'>
       <Header />
       <Elementor />
+      <div className='main-content'>
+      <MainSlider />
+      </div>
     </section>
   )
 }
---
+++ src/components/MainSlider/MainSlider.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SimpleSlider from '../slider/slider'
+
+function MainSlider() {
+  return (
+    <div>
+      <SimpleSlider />
+    </div>
+  )
+}
+
+export default MainSlider
