98a91011d47d060f1ce4587dd297175082fc7a74
ElGutiar <el12guitar@gmail.com>
2021-05-31 11:18:58 +0300

    Markup PlayerInfo page top row

--- src/Routes/PlayerInfo/PlayerInfo.tsx
+++ src/Routes/PlayerInfo/PlayerInfo.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
 
-import './player.scss'
+import './playerInfo.scss'
 
 function PlayerInfo() {
   return (
-    <div>
-      red
-    </div>
+    <section className='player-page'>
+      <div className='player-page__wrapper'>
+        <div className='player-page__stats-row'>
+          <div className='player-page__player-image-wrapper'>
+            <img
+              className='player-page__player-image-wrapper--image'
+              src="/"
+              alt="/"
+            />
+          </div>
+
+          <div className='player-page__player-info'>
+            <h3 className='player-page__player-info--nickname'>
+
+            </h3>
+
+            <h4 className='player-page__player-info--role'>
+
+            </h4>
+          </div>
+
+          <div className='player-page__player-social'>
+            <ul className='player-page__player-social--list'>
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
